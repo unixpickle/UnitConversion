525448e30dc27e30695ec8a511cc986106865ece
Alex Nichol <alexnichol@comcast.net>
2011-10-24 18:48:18 -0400

    Initial Commit

---
+++ UnitConversion/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  UnitConversion
+//
+//  Created by Alex Nichol on 10/24/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
